8fe4c644d3e53a859fd41edd4ff8d145278580e5
Suresh Marru <smarru@apache.org>
2017-09-12 16:07:38 -0400

    updating copyright year

--- views/landingpage.blade.php
+++ views/landingpage.blade.php
@@ -129,7 +129,7 @@
                     </p>
                     
                     <p class="copyright">
-                        <a href="https://www.iu.edu/copyright/index.html">Copyright</a> &#169; 2016 <span class="line-break-small">The Trustees of <a href="https://www.iu.edu/" itemprop="url"><span itemprop="name">Indiana University</span></a></span><span class="hide-on-mobile"> | </span><span class="line-break"><a href="/privacy" id="privacy-policy-link">Privacy Notice</a> | <a href="https://accessibility.iu.edu" id="accessibility-link" title="Having trouble accessing this web page because of a disability? Visit this page for assistance.">Accessibility</a></span>
+                        <a href="https://www.iu.edu/copyright/index.html">Copyright</a> &#169; 2017 <span class="line-break-small">The Trustees of <a href="https://www.iu.edu/" itemprop="url"><span itemprop="name">Indiana University</span></a></span><span class="hide-on-mobile"> | </span><span class="line-break"><a href="/privacy" id="privacy-policy-link">Privacy Notice</a> | <a href="https://accessibility.iu.edu" id="accessibility-link" title="Having trouble accessing this web page because of a disability? Visit this page for assistance.">Accessibility</a></span>
                     </p>
                 </div>
             </footer>
